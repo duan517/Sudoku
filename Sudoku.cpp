83d1c5affa9e489c7ae2d39eed719619980e8f7b
duan517 <duan517@users.noreply.github.com>
2021-08-09 10:42:53 +0800

    添加注释，优化结束条件。

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -9,9 +9,9 @@
 
 using namespace std;
 
-void PrintMatrix(int *matrix)
+void PrintMatrix(int* matrix)
 {
-	cout << setw(2) << " ";
+	cout << endl << setw(2) << " ";
 	for (int i = 0; i < COLS; i++)
 	{
 		cout << setw(2) << i + 1;
@@ -30,28 +30,18 @@ void PrintMatrix(int *matrix)
 	}
 }
 
-inline bool IsComplete(int *matrix)
-{
-	for (int i = 0; i < COLS * ROWS; i++)
-	{
-		if (*(matrix + i) == 0)
-		{
-			return false;
-		}
-	}
-	return true;
-}
-
-bool TryNext(int *matrix, int offset = 0)
+bool TryNext(int* matrix, int offset = 0)
 {
 	for (int i = offset; i < COLS * ROWS; i++)
 	{
 		if (*(matrix + i) == 0)
 		{
+			// 逐个尝试所有可能的数字。
 			for (int n = 1; n <= COLS; n++)
 			{
 				bool exists = false;
 
+				// 检查所在行是否已经填过该数字了。
 				for (int x = i / COLS * COLS; !exists && x < (i / COLS + 1) * COLS; x++)
 				{
 					if (*(matrix + x) == n)
@@ -60,6 +50,7 @@ bool TryNext(int *matrix, int offset = 0)
 					}
 				}
 
+				// 检查所在列是否已经填过该数字了。
 				for (int y = i % COLS; !exists && y < COLS * ROWS; y += COLS)
 				{
 					if (*(matrix + y) == n)
@@ -68,7 +59,9 @@ bool TryNext(int *matrix, int offset = 0)
 					}
 				}
 
+				// 计算当前位置所在九宫格左上角的偏移量。
 				int delta = i / (COLS * BLOCK_ROWS) * (COLS * BLOCK_ROWS) + i % COLS / BLOCK_COLS * BLOCK_COLS;
+				// 检查所在九宫格是否已经填过该数字了。
 				for (int y = 0; !exists && y < BLOCK_ROWS; y++)
 				{
 					for (int x = 0; !exists && x < BLOCK_COLS; x++)
@@ -83,14 +76,8 @@ bool TryNext(int *matrix, int offset = 0)
 				if (!exists)
 				{
 					*(matrix + i) = n;
-					if (!TryNext(matrix, i + 1))
-					{
-						if (IsComplete(matrix))
-						{
-							return true;
-						}
-					}
-					else
+					// 已经填了最后一个数，或者后续调用找到解，则返回成功标志。
+					if (i == COLS * ROWS - 1 || TryNext(matrix, i + 1))
 					{
 						return true;
 					}
@@ -108,7 +95,7 @@ void DisplayMenu()
 	cout << "[0]Blank, [P]rint Matrix, [C]lear Line, [R]eset Matrix, [H]elp, [Q]uit" << endl;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	int matrix[COLS * ROWS] = { 0 };
 
@@ -117,7 +104,7 @@ int main(int argc, char *argv[])
 	DisplayMenu();
 
 	char buffer[COLS * ROWS + 1];
-	for (int i = 0; i < COLS * ROWS;)
+	for (int i = 0; i < COLS * ROWS && !cin.eof();)
 	{
 		cout << (char)(i / COLS + 'A') << i % COLS + 1 << ">";
 		cin.getline(buffer, sizeof(buffer));
